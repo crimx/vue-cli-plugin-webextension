2d43c2a55aa844ec27d00ece58093728587cda65
CRIMX <straybugs@gmail.com>
2019-02-14 18:29:55 +0800

    auto gen browser list

--- generator/template/scripts/browsers.js
+++ generator/template/scripts/browsers.js
@@ -1,1 +1,7 @@
-module.exports = ['chrome', 'firefox']
+const fs = require('fs')
+const path = require('path')
+
+module.exports = fs
+  .readdirSync(path.join(__dirname, '../src/manifest'))
+  .map(name => (/^(\S+)\.manifest\./.exec(name) || ['', ''])[1])
+  .filter(name => name && name !== 'common')
